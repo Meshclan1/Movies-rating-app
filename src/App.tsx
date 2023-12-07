acac761d4f1bba31e24ba936583e0e490d8a0431
Ayo <Ayo.meshe@gmail.com>
2023-12-07 12:30:27 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
