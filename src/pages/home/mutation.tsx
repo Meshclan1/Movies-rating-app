9c15c6e403210a59068b3682e3ddc31a24a66b40
Ayo <Ayo.meshe@gmail.com>
2023-12-28 19:54:37 +0000

    Update mutation.tsx

--- src/pages/home/mutation.tsx
+++ src/pages/home/mutation.tsx
@@ -1,4 +1,7 @@
 export const rateMovie = async (movieId: number, rating: number) => {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  console.log("API Key:", apiKey);
+
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${localStorage.getItem(
       "guest_session_id"
