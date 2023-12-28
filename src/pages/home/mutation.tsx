dd6640194e3b6bcd8b65e9299b1e99b5e3e0f2aa
Ayo <Ayo.meshe@gmail.com>
2023-12-28 19:00:16 +0000

    app bug fix

--- src/pages/home/mutation.tsx
+++ src/pages/home/mutation.tsx
@@ -1,9 +1,8 @@
 export const rateMovie = async (movieId: number, rating: number) => {
-  const apiKey = import.meta.env.VITE_API_KEY;
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${localStorage.getItem(
       "guest_session_id"
-    )}&api_key=${apiKey}`,
+    )}&api_key=${import.meta.env.VITE_API_KEY}`,
     {
       method: "POST",
       headers: {
@@ -18,11 +17,10 @@ export const rateMovie = async (movieId: number, rating: number) => {
 };
 
 export const rateTvShow = async (tvShowId: number, rating: number) => {
-  const apiKey = import.meta.env.VITE_API_KEY;
   const res = await fetch(
     `https://api.themoviedb.org/3/tv/${tvShowId}/rating?guest_session_id=${localStorage.getItem(
       "guest_session_id"
-    )}&api_key=${apiKey}`,
+    )}&api_key=${import.meta.env.VITE_API_KEY}`,
     {
       method: "POST",
       headers: {
--- src/pages/rated/query.tsx
+++ src/pages/rated/query.tsx
@@ -1,20 +1,22 @@
 export const fetchRatedMovies = async () => {
-  const apiKey = import.meta.env.VITE_API_KEY;
   const res = await fetch(
     `https://api.themoviedb.org/3/guest_session/${localStorage.getItem(
       "guest_session_id"
-    )}/rated/movies?language=en-US&page=1&sort_by=created_at.asc&api_key=${apiKey}`
+    )}/rated/movies?language=en-US&page=1&sort_by=created_at.asc&api_key=${
+      import.meta.env.VITE_API_KEY
+    }`
   );
 
   return res.json();
 };
 
 export const fetchRatedTvShows = async () => {
-  const apiKey = import.meta.env.VITE_API_KEY;
   const res = await fetch(
     `https://api.themoviedb.org/3/guest_session/${localStorage.getItem(
       "guest_session_id"
-    )}/rated/tv?language=en-US&page=1&sort_by=created_at.asc&api_key=${apiKey}`
+    )}/rated/tv?language=en-US&page=1&sort_by=created_at.asc&api_key=${
+      import.meta.env.VITE_API_KEY
+    }`
   );
 
   return res.json();
